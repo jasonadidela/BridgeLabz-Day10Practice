da21a73c773f1002d9b840a37f1fcc9a2925d1e3
jasonadidela <jasonadidela@gmail.com>
2022-11-27 23:22:31 +0530

    final Code for AddressBook.

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program in AddressBookMain class on Master Branch");
+
+        do {
+            System.out.println("Enter:[D/d]-Display Contacts" + "\s" + "[A/a]-Add Contact " + "\s" + "[E/e]-Edit Contact" + "\s" +
+                    "[R/r]-Remove Contact");
+            Scanner value = new Scanner(System.in);
+            char input = value.next().charAt(0);
+            switch (input) {
+                case 'D', 'd' -> AddressBook.printContact();
+                case 'A', 'a' -> AddressBook.addContact();
+                case 'R', 'r' -> AddressBook.removeContact();
+                case 'E', 'e' -> AddressBook.editContact();
+            }
+        } while (true);
     }
 }
+
