e137ea7a112f8c516f6eaadcc91c5f1ead12bbbe
jasonadidela <jasonadidela@gmail.com>
2022-11-27 23:59:33 +0530

    final Code for AddressBook.

--- AddressBook.java
+++ AddressBook.java
@@ -1,13 +1,18 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 public class AddressBook {
-    Scanner s = new Scanner(System.in);
-    ArrayList<Person> list = new ArrayList<Person>();
-    public void add() {
+
+    static ArrayList<Person> list = new ArrayList<>();
+
+    public static void addContact() {
+
+        Scanner s = new Scanner(System.in);
         Person person = new Person();
+
         System.out.println("Enter the First name:");
         String firstName = s.next();
         person.setFirstName(firstName);
+        list.add(person);
 
         System.out.println("Enter the Last name:");
         String lastName = s.next();
@@ -37,7 +42,70 @@ public class AddressBook {
         String email = s.next();
         person.setEmail(email);
 
-        list.add(person);
+    }
 
+    public static void editContact() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your First name:");
+        String firstName = s.next();
+
+        for (Person person : list) {
+            if (firstName.equals(person.getFirstName())) {
+                System.out.println("Choose field you want to add:");
+                System.out
+                        .println("1.Last Name\t2.City\t3.State\t4.Zip\t5.Phone Number\t6.Email ");
+                switch (s.nextInt()) {
+                    case 1 -> {
+                        System.out.println("Re-Correct your lastname");
+                        person.setLastName(s.next());
+                    }
+                    case 2 -> {
+                        System.out.println("Re-Correct your City");
+                        person.setCity(s.next());
+                    }
+                    case 3 -> {
+                        System.out.println("Re-Correct your State");
+                        person.setState(s.next());
+                    }
+                    case 4 -> {
+                        System.out.println("Re-Correct your Zip");
+                        person.setZipCode(s.next());
+                    }
+                    case 5 -> {
+                        System.out.println("Re-Correct your Phone Number");
+                        person.setPhoneNumber(s.next());
+                    }
+                    case 6 -> {
+                        System.out.println("Re-Correct your Email");
+                        person.setEmail(s.next());
+                    }
+                }
+
+            }
+        }
+
+    }
+
+    public static void removeContact() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your First name:");
+        String firstName = s.next();
+
+        list.removeIf(person -> firstName.equals(person.getFirstName()));
+    }
+
+    public static void printContact() {
+        for (Person person : list) {
+            System.out.println("**********************************************"+
+                    "\nFirst Name:\s"+ person.getFirstName()+
+                    "\nLast Name:\s"+person.getLastName()+
+                    "\nAddress:\s"+person.getAddress()+
+                    "\nCity:\s"+person.getCity()+
+                    "\nState:\s"+person.getState()+
+                    "\nZip Code:\s"+person.getZipCode()+
+                    "\nPhone No:\s"+person.getPhoneNumber()+
+                    "\nEmail:\s"+person.getEmail()+
+                    "\n**********************************************");
+        }
     }
 }
