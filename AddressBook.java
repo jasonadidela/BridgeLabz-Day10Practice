6151ad5c61c275cb7b0125730e01c49ed51ee2b7
jasonadidela <jasonadidela@gmail.com>
2022-12-13 02:22:48 +0530

    by using HashMap AddressBook is put the variables and get the variables.

--- AddressBook.java
+++ AddressBook.java
@@ -1,126 +1,45 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
+
 public class AddressBook {
-
-    static ArrayList<Person> list = new ArrayList<>();// The new keyword is Allocating Desired Memory for "list"(Object) in Person class.
-           // ArrayList is a class used for storing elements, its creating new generic arraylist.
-
+    static  HashMap<String, String> map = new HashMap<>();
+    static Scanner s = new Scanner(System.in);
+    static Person contact = new Person();
     public static void addContact() {
-
-        Scanner s = new Scanner(System.in);
-        Person contact = new Person();              // The new keyword is Allocating Desired Memory for "contact"(Object) in "Person" class.
-        list.add(contact);                          //It is used to append "contact" elements at the end of a "list".
-
         System.out.println("Enter the First name:");
-        String firstName = s.next();
-        contact.setFirstName(firstName);
-
+        map.put(contact.setFirstName(), s.nextLine());
         System.out.println("Enter the Last name:");
-        String lastName = s.next();
-        contact.setLastName(lastName);
-
+        map.put(contact.setLastName(),s.nextLine());
         System.out.println("Enter the Address:");
-        String address = s.next();
-        contact.setAddress(address);
-
+        map.put(contact.setAddress(),s.nextLine());
         System.out.println("Enter the City:");
-        String city = s.next();
-        contact.setCity(city);
-
+        map.put(contact.setCity(),s.nextLine());
         System.out.println("Enter the State:");
-        String state = s.next();
-        contact.setState(state);
-
+        map.put(contact.setState(),s.nextLine());
         System.out.println("Enter the Zip:");
-        String zipCode = s.next();
-        contact.setZipCode(zipCode);
-
+        map.put(contact.setZipCode(),s.nextLine());
         System.out.println("Enter the Phone Number:");
-        String phoneNumber = s.next();
-        contact.setPhoneNumber(phoneNumber);
-
+        map.put(contact.setPhoneNumber(),s.nextLine());
         System.out.println("Enter the Email:");
-        String email = s.next();
-        contact.setEmail(email);
+        map.put(contact.setEmail(),s.nextLine());
 
     }
-
-    public static void editContact() {              // Created Object called "editContact", and is called in "AddersBookMain.java" class by main method.
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter your First name:");
-        String firstName = s.next();
-
-        for (Person person : list) {
-            if (firstName.equals(person.getFirstName())) {
-                int i = 1;
-                while (i<=3){
-                    System.out.println("Choose field you want to add:");
-                    System.out.println("[L/l]Last Name\s[C/c]City\s[S/s]State\s[Z/z]Zip\s[P/p]Phone Number\s[E/e]Email ");
-                    char value = s.next().charAt(0);
-                    if (value == 'L' || value == 'l' || value == 'c' || value == 'C' || value == 's' || value == 'S' ||
-                            value == 'z' || value == 'Z' || value == 'p' || value == 'P' || value == 'e' || value == 'E') {
-                        switch (value) {
-                            case 'L', 'l' -> {
-                                System.out.println("Re-Correct your lastname");
-                                person.setLastName(s.next());
-                            }
-                            case 'c', 'C' -> {
-                                System.out.println("Re-Correct your City");
-                                person.setCity(s.next());
-                            }
-                            case 's', 'S' -> {
-                                System.out.println("Re-Correct your State");
-                                person.setState(s.next());
-                            }
-                            case 'z', 'Z' -> {
-                                System.out.println("Re-Correct your Zip");
-                                person.setZipCode(s.next());
-                            }
-                            case 'p', 'P' -> {
-                                System.out.println("Re-Correct your Phone Number");
-                                person.setPhoneNumber(s.next());
-                            }
-                            case 'e', 'E' -> {
-                                System.out.println("Re-Correct your Email");
-                                person.setEmail(s.next());
-                            }
-                        }
-                    } else {
-                        System.out.println("Error("+i+"): invalid input entered for the interstate question");
-                        i++;
-                    }
-                    System.out.println("Too many args Edite ended!");
-                }
-            }
-        }
-    }
-
-    public static void removeContact() {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter your First name:");
-        String firstName = s.next();
-
-        list.removeIf(person -> firstName.equals(person.getFirstName()));
-
-    }
-
     public static void printContact() {
-        for (Person person : list) {
-            int i = 1;
-            i++;
-            System.out.println("**********************************************"+
-                    "\nContact:\s"+ list.get(i)+
-                    "\nFirst Name:\s"+ person.getFirstName()+
-                    "\nLast Name:\s"+person.getLastName()+
-                    "\nAddress:\s"+person.getAddress()+
-                    "\nCity:\s"+person.getCity()+
-                    "\nState:\s"+person.getState()+
-                    "\nZip Code:\s"+person.getZipCode()+
-                    "\nPhone No:\s"+person.getPhoneNumber()+
-                    "\nEmail:\s"+person.getEmail()+
-                    "\n**********************************************"+
-                    "\nTotal Number of Contacts:\s" + list.size()+
-                    "\n**********************************************");
+        System.out.println("**********************************************"+
+                "\nFirst Name:\s"+ map.get(contact.getFirstName())+
+                "\nLast Name:\s"+map.get(contact.getLastName())+
+                "\nAddress:\s"+map.get(contact.getAddress())+
+                "\nCity:\s"+map.get(contact.getCity())+
+                "\nState:\s"+map.get(contact.getState())+
+                "\nZip Code:\s"+map.get(contact.getZipCode())+
+                "\nPhone No:\s"+map.get(contact.getPhoneNumber())+
+                "\nEmail:\s"+map.get(contact.getEmail())+
+                "\n**********************************************"+
+                "\nTotal Number of Contacts:\s" + map.size()+
+                "\n**********************************************");
         }
+
+    public static void main(String[] args) {
+        addContact();
+        printContact();
     }
 }
