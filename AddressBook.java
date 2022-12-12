7f9b7a62c8831ba62f97ad6dd0b486f7d4cbb6ab
jasonadidela <jasonadidela@gmail.com>
2022-12-13 02:34:05 +0530

    editContact method is created by using HashMap in AddressBook.

--- AddressBook.java
+++ AddressBook.java
@@ -38,8 +38,57 @@ public class AddressBook {
                 "\n**********************************************");
         }
 
+    public static void editContact(){
+        System.out.println("Enter your First name:");
+        String firstName = s.nextLine();
+        if (firstName.equals(map.get(contact.getFirstName()))){
+            int i = 1;
+            while (i<=3){
+                System.out.println("Choose field you want to add:");
+                System.out.println("[L/l]Last Name\s[C/c]City\s[S/s]State\s[Z/z]Zip\s[P/p]Phone Number\s[E/e]Email ");
+                char value = s.next().charAt(0);
+                if (value == 'L' || value == 'l' || value == 'c' || value == 'C' || value == 's' || value == 'S' ||
+                        value == 'z' || value == 'Z' || value == 'p' || value == 'P' || value == 'e' || value == 'E') {
+                    switch (value) {
+                        case 'L', 'l' -> {
+                            System.out.println("Re-Correct your lastname");
+                            contact.setLastName(s.next());
+                        }
+                        case 'c', 'C' -> {
+                            System.out.println("Re-Correct your City");
+                            contact.setCity(s.next());
+                        }
+                        case 's', 'S' -> {
+                            System.out.println("Re-Correct your State");
+                            contact.setState(s.next());
+                        }
+                        case 'z', 'Z' -> {
+                            System.out.println("Re-Correct your Zip");
+                            contact.setZipCode(s.next());
+                        }
+                        case 'p', 'P' -> {
+                            System.out.println("Re-Correct your Phone Number");
+                            contact.setPhoneNumber(s.next());
+                        }
+                        case 'e', 'E' -> {
+                            System.out.println("Re-Correct your Email");
+                            contact.setEmail(s.next());
+                        }
+                    }
+                } else {
+                    System.out.println("Error("+i+"): invalid input entered for the interstate question");
+                    i++;
+                }
+                System.out.println("Too many args Edite ended!");
+            }
+
+        }
+    }
+
+
     public static void main(String[] args) {
         addContact();
         printContact();
+        editContact();
     }
 }
