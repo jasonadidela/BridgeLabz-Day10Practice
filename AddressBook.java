c3a7e2e87708a6627332f07aec8f8e612ebb4be6
jasonadidela <jasonadidela@gmail.com>
2022-12-13 06:04:01 +0530

    editContact method is created by using HashMap in AddressBook.

--- AddressBook.java
+++ AddressBook.java
@@ -1,94 +1,104 @@
 import java.util.*;
 
 public class AddressBook {
-    static  HashMap<String, String> map = new HashMap<>();
+
     static Scanner s = new Scanner(System.in);
-    static Person contact = new Person();
+    static ArrayList<Person> contact =new ArrayList<>();
+    static HashMap<String, ArrayList<Person>> map = new HashMap<>();
+    static Person person = new Person();
+
+
     public static void addContact() {
+        contact.add(person);
+        map.put("addressBook",contact);
+        System.out.println("Enter details : ");
         System.out.println("Enter the First name:");
-        map.put(contact.setFirstName(), s.nextLine());
+        String firstName = s.next();
+        person.setFirstName(firstName);
         System.out.println("Enter the Last name:");
-        map.put(contact.setLastName(),s.nextLine());
+        String lastName = s.next();
+        person.setLastName(lastName);
         System.out.println("Enter the Address:");
-        map.put(contact.setAddress(),s.nextLine());
+        String Address = s.next();
+        person.setAddress(Address);
         System.out.println("Enter the City:");
-        map.put(contact.setCity(),s.nextLine());
+        String city = s.next();
+        person.setCity(city);
         System.out.println("Enter the State:");
-        map.put(contact.setState(),s.nextLine());
+        String state = s.next();
+        person.setState(state);
         System.out.println("Enter the Zip:");
-        map.put(contact.setZipCode(),s.nextLine());
+        String zip = s.next();
+        person.setZipCode(zip);
         System.out.println("Enter the Phone Number:");
-        map.put(contact.setPhoneNumber(),s.nextLine());
+        String number = s.next();
+        person.setPhoneNumber(number);
         System.out.println("Enter the Email:");
-        map.put(contact.setEmail(),s.nextLine());
+        String email = s.next();
+        person.setEmail(email);
 
     }
     public static void printContact() {
-        System.out.println("**********************************************"+
-                "\nFirst Name:\s"+ map.get(contact.getFirstName())+
-                "\nLast Name:\s"+map.get(contact.getLastName())+
-                "\nAddress:\s"+map.get(contact.getAddress())+
-                "\nCity:\s"+map.get(contact.getCity())+
-                "\nState:\s"+map.get(contact.getState())+
-                "\nZip Code:\s"+map.get(contact.getZipCode())+
-                "\nPhone No:\s"+map.get(contact.getPhoneNumber())+
-                "\nEmail:\s"+map.get(contact.getEmail())+
-                "\n**********************************************"+
-                "\nTotal Number of Contacts:\s" + map.size()+
-                "\n**********************************************");
+        for (Person person : contact) {
+            System.out.println(person);
         }
+    }
 
     public static void editContact(){
         System.out.println("Enter your First name:");
-        String firstName = s.nextLine();
-        if (firstName.equals(map.get(contact.getFirstName()))){
-            int i = 1;
-            while (i<=3){
-                System.out.println("Choose field you want to add:");
-                System.out.println("[L/l]Last Name\s[C/c]City\s[S/s]State\s[Z/z]Zip\s[P/p]Phone Number\s[E/e]Email ");
-                char value = s.next().charAt(0);
-                if (value == 'L' || value == 'l' || value == 'c' || value == 'C' || value == 's' || value == 'S' ||
-                        value == 'z' || value == 'Z' || value == 'p' || value == 'P' || value == 'e' || value == 'E') {
-                    switch (value) {
-                        case 'L', 'l' -> {
-                            System.out.println("Re-Correct your lastname");
-                            contact.setLastName(s.next());
-                        }
-                        case 'c', 'C' -> {
-                            System.out.println("Re-Correct your City");
-                            contact.setCity(s.next());
-                        }
-                        case 's', 'S' -> {
-                            System.out.println("Re-Correct your State");
-                            contact.setState(s.next());
-                        }
-                        case 'z', 'Z' -> {
-                            System.out.println("Re-Correct your Zip");
-                            contact.setZipCode(s.next());
-                        }
-                        case 'p', 'P' -> {
-                            System.out.println("Re-Correct your Phone Number");
-                            contact.setPhoneNumber(s.next());
-                        }
-                        case 'e', 'E' -> {
-                            System.out.println("Re-Correct your Email");
-                            contact.setEmail(s.next());
-                        }
+        String firstName;
+        firstName = s.next();
+        if (firstName.equals(person.getFirstName())){
+            System.out.println("Choose field you want to add:");
+            System.out.println("[L/l]Last Name\s[C/c]City\s[S/s]State\s[Z/z]Zip\s[P/p]Phone Number\s[E/e]Email ");
+            char value = s.next().charAt(0);
+            if (value == 'L' || value == 'l' || value == 'c' || value == 'C' || value == 's' || value == 'S' ||
+                    value == 'z' || value == 'Z' || value == 'p' || value == 'P' || value == 'e' || value == 'E') {
+                switch (value) {
+                    case 'L', 'l' -> {
+                        System.out.println("Re-Correct your lastname");
+                        String lastName = s.next();
+                        person.setLastName(lastName);
                     }
-                } else {
-                    System.out.println("Error("+i+"): invalid input entered for the interstate question");
-                    i++;
+                    case 'c', 'C' -> {
+                        System.out.println("Re-Correct your City");
+                        String city = s.next();
+                        person.setCity(city);
+                    }
+                    case 's', 'S' -> {
+                        System.out.println("Re-Correct your State");
+                        String state = s.next();
+                        person.setState(state);
+                    }
+                    case 'z', 'Z' -> {
+                        System.out.println("Re-Correct your Zip");
+                        String zip = s.next();
+                        person.setZipCode(zip);
+                    }
+                    case 'p', 'P' -> {
+                        System.out.println("Re-Correct your Phone Number");
+                        String phone = s.next();
+                        person.setPhoneNumber(phone);
+                    }
+                    case 'e', 'E' -> {
+                        System.out.println("Re-Correct your Email");
+                        String email = s.next();
+                        person.setEmail(email);
+                    }
                 }
-                System.out.println("Too many args Edite ended!");
             }
-
         }
     }
 
-
-    public static void main(String[] args) {
-        addContact();
-        printContact();
-        editContact();
+    public static void removeContact() {
+        System.out.println("Enter name to delete : ");
+        String name = s.next();
+        for (Person person : contact) {
+            if (person.getFirstName().equals(name)) {
+                contact.remove(person);
+                System.out.println("Contact deleted successfully !!!");
+            }
+        }
     }
+
 }
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,20 +1,26 @@
 import java.util.Scanner;
 
 public class AddressBookMain {
+
     public static void main(String[] args) {
         int i = 1;
         while (i<=3){
-            System.out.println("Enter:[D/d]-Display Contacts" + "\s" + "[A/a]-Add Contact " + "\s" +
-                    "[E/e]-Edit Contact" + "\s" + "[R/r]-Remove Contact");
+            System.out.println("Enter:[D/d]-Display Contacts" + "\s" +
+                    "[B/b]-Add New AddressBook " + "\s" +
+                    "[E/e]-Edit Contact" + "\s" +
+                    "[A/a]-Add Contact " + "\s" +
+                    "[R/r]-Remove Contact");
             Scanner value = new Scanner(System.in);
             char input = value.next().charAt(0);
             if (input == 'D' || input == 'd' || input == 'A' || input == 'a' ||
-                    input == 'R' || input == 'r' || input == 'E' || input == 'e') {
+                    input == 'B' || input == 'b' || input == 'R' || input == 'r' || input == 'E' || input == 'e') {
                 switch (input) {
-                    case 'D', 'd' -> AddressBook.printContact();
                     case 'A', 'a' -> AddressBook.addContact();
+                    case 'D', 'd' -> AddressBook.printContact();
+//                    case 'B', 'b' -> AddressBook.addContact();
                     case 'R', 'r' -> AddressBook.removeContact();
                     case 'E', 'e' -> AddressBook.editContact();
+
                 }
             } else {
                 System.out.println("Error("+i+"): invalid input entered for the interstate question");
